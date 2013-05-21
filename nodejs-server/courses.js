3b1fe40e47e62f44670fd9e52a58918a9331d92a
mrgamer <mister.gamer@gmail.com>
2013-05-21 12:51:10 +0200

    nodejs server included

---
+++ nodejs-server/courses.js
@@ -0,0 +1,13 @@
+/**
+  * 
+  * Courses REST API file
+  * 
+ */
+
+exports.getcourse = function (req, res, next) {
+	res.send(200);
+};
+
+exports.postcourse = function (req, res, next) {
+	res.send(200);
+};
