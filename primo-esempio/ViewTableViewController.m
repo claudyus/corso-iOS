089f550f81a4d9a8b0401420b9a42c19835c3743
sviluppatore1 <sviluppatore1@MacMini1.local>
2013-05-21 10:17:36 +0200

    metodi delegati della tabella

--- primo-esempio/ViewTableViewController.m
+++ primo-esempio/ViewTableViewController.m
@@ -39,4 +39,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma callback della tabella
+/* quante sezioni ci sono nella tabella ?? */
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+// Setta il numero di righe della tabella
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [arrayView count];
+}
+
+// Setta il contenuto delle varie celle
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
+    
+    if (cell == nil){
+        cell = [[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"cellID"] ;
+        //setta lo stile con cui vengono selezionate le righe
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    //inseriamo nella cello l'elemento della lista corrispondente
+    cell.textLabel.text = [arrayView objectAtIndex:indexPath.row];
+    cell.subviews
+    return cell;
+}
+
+
+
+
 @end
